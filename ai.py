ee0f433cb8b4182c0d39ccd2c5e72256a54d2528
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-12-09 02:00:01 +0100

    Basic assistant

---
+++ ai.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from mistralai import Mistral
+
+
+class ChatBody:
+
+    def __init__(self, system_prompt: str, question: str, history: List[str], prefix: str):
+        self.system_prompt = system_prompt
+        self.question = question
+        self.history = history
+        self.prefix = prefix
+
+    def body(self):
+        body = [{ "role": "system", "content": self.system_prompt }]
+        body += self.history
+        body.append({ "role": "user", "content": self.question })
+        if self.prefix:
+            body.append({ "role": "assistant", "content": self.prefix, "prefix": True })  # pyright: ignore [reportArgumentType]
+        return body
+
+
+class Assistant:
+
+    def __init__(self, client: Mistral):
+        self.client = client
+
+    def __call__(self, *args):
+        return self.ask(*args)
+
+    def ask(self, question: str, history: List[str], mistral_model: str, system_prompt: str, temperature: float, prefix: str):
+        chat_body = ChatBody(system_prompt, question, history, prefix)
+        chat_response = self.client.chat.complete(
+            model=mistral_model,
+            messages=chat_body.body(),  # pyright: ignore [reportArgumentType]
+            temperature=temperature
+        )
+
+        if chat_response and chat_response.choices:
+            return chat_response.choices[0].message.content
+        else:
+            raise Exception(f'Bad chat response {chat_response}')
