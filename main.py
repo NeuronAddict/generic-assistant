4aa86703fca23b0257544389fc9f970849d865e9
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-12-20 01:41:22 +0100

    Review gradio interface

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ai_session: AISession | None = None
 def chat(question: str, _: List[str], model: str, system_prompt: str, temperature: float, prefix: str) -> str:
     global ai_session
     if ai_session is None:
-        ai_session = LogAISession(BaseAISession( BaseModel(client, model), system_prompt, temperature, prefix))
+        ai_session = LogAISession(BaseAISession(BaseModel(client, model), system_prompt, temperature, prefix))
     return ai_session.ask(question)
 
 if __name__ == '__main__':
@@ -29,11 +29,12 @@ if __name__ == '__main__':
                 temp_slider = gr.Slider(0, 1, value=0.2, label="Temperature",
                                     info="Choose temperature https://docs.mistral.ai/api/#tag/chat/operation/chat_completion_v1_chat_completions_post")
 
+                system_prompt = gr.Textbox(label="System prompt", lines=3,
+                                       value='Vous êtes un assistant utile et courtois. Vous répondez avec des références à des sources fiables. Vous répondez en francais.')
+                prefix = gr.Textbox(label="Prefix", lines=3,
+                                value='Réponse en français de l\'assistant, avec des sources fiables :')
 
             with gr.Column(scale=4, variant='panel'):
-                with gr.Row():
-                    system_prompt = gr.Textbox(label="System prompt", lines=3, value='Vous êtes un assistant utile et courtois. Vous répondez avec des références à des sources fiables. Vous répondez en francais.')
-                    prefix = gr.Textbox(label="Prefix", lines=3, value='Réponse en français de l\'assistant, avec des sources fiables :')
 
                 gr.ChatInterface(
                     chat, additional_inputs=[model_choose, system_prompt, temp_slider, prefix], type="messages",
